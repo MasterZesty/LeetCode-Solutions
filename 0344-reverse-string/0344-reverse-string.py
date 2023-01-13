8d4cfb5b94a494e97dda4bb6553fa5b6080de199
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-01-13 23:16:48 +0530

    Time: 260 ms (50.78%), Space: 16 MB (99.99%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        j = len(s)
+        
+        for i in range(len(s)):
+            
+            print(i,j,s[i],s[j-1],j//2)
+            
+            if (i == j) or (len(s)//2 == i):
+                print("breking",i,j)
+                break
+                
+            s[i], s[j-1] = s[j-1], s[i]
+            
+            j -= 1
+        
