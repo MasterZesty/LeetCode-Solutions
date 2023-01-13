366b9f38dc77af086eb3159d07808c2c7f498632
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-01-13 23:17:00 +0530

    Time: 211 ms (69.16%), Space: 18.4 MB (83.23%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -7,10 +7,10 @@ class Solution:
         
         for i in range(len(s)):
             
-            print(i,j,s[i],s[j-1],j//2)
+            # print(i,j,s[i],s[j-1],j//2)
             
             if (i == j) or (len(s)//2 == i):
-                print("breking",i,j)
+                # print("breking",i,j)
                 break
                 
             s[i], s[j-1] = s[j-1], s[i]
