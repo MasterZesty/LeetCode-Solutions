9fcf45b57445bb7be0e9714026342ac7d73ab39a
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-02-22 00:35:23 +0530

    Time: 253 ms (12.32%), Space: 23.7 MB (39.30%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(0,len(nums)-1,2):
             if nums[i] != nums[i+1]:
                 
-                print(nums[i],nums[i+1])
+                # print(nums[i],nums[i+1])
                 return nums[i]
             
         return nums[-1]
