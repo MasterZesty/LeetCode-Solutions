c0bbc815e5cf633f003078ea79d8d5b33fde7c56
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-07-10 23:12:44 +0530

    Time: 239 ms (57.84%), Space: 16.4 MB (8.92%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.py
@@ -1,6 +1,8 @@
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
-        # approch one O(n2)
+        # approch 1 
+        # time complexity - O(n2)
+        # space complexity O(1)
         
         n = len(nums)
         ans = 0
@@ -10,7 +12,7 @@ class Solution:
             for j in range(i+1,n):
                 
                 if abs(nums[i]-nums[j]) == k:
-                    print(nums[i],nums[j])
+                    # print(nums[i],nums[j])
                     ans += 1
                 
         return ans
