33d8dd1a2e54d1de591e822a239303e5a04bebaa
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-08-29 01:28:46 +0530

    Time: 42 ms (42.41%), Space: 16.3 MB (82.88%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,33 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        hm = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        
+        combinations = []
+        
+        n = len(digits)
+        
+        if n == 0:
+            return []
+        
+        if n == 1:
+            return [x for x in  hm[digits]]
+        
+        def combine(terms, accum):
+            
+            last = (len(terms) == 1)
+            n = len(terms[0])
+            for i in range(n):
+                item = accum + terms[0][i]
+                if last:
+                    combinations.append(item)
+                else:
+                    combine(terms[1:], item)
+        
+        terms = []
+        for i in digits:
+            terms.append([x for x in hm[i]])
+            
+        combine(terms, '')
+        
+        return combinations
