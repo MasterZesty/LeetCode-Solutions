2f9f0eb595e8212079d73686a8c07725832c9f68
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-09-05 23:43:33 +0530

    Time: 53 ms (88.45%), Space: 20.2 MB (87.58%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Approch 1 : Floyd’s Cycle Finding Algorithm - Floyd’s cycle finding algorithm 
+        # or Hare-Tortoise algorithm is a pointer algorithm that uses only two pointers, 
+        # moving through the sequence at different speeds. This algorithm is used to find 
+        # a loop in a linked list. It uses two pointers one moving twice as fast as the 
+        # other one. The faster one is called the fast pointer and the other one is called 
+        #the slow pointer.
+        # ref : https://www.geeksforgeeks.org/floyds-cycle-finding-algorithm/
+        
+        slowptr = head
+        fastptr = head
+        
+        while (slowptr != None and fastptr != None and fastptr.next != None):
+            slowptr = slowptr.next
+            fastptr = fastptr.next.next
+            
+            if slowptr == fastptr:
+                return True
+            
+        return False
+            
