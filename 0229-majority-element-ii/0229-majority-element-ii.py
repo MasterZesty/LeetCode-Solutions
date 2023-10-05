2fd6a36acee74906435831e0e611adbb20a86eef
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-10-05 19:04:12 +0530

    Time: 117 ms (28.55%), Space: 17.6 MB (97.53%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,6 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         # approch 1 : TC O(n)
+        # note : if you use list and check k in list TC is O(n) for set it is O(1) hence used 
+        # set not list as ans
         n =len(nums)
         t = n // 3
         ans = set()
