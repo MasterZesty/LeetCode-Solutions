1b0510f61fd8279f0f30aa32580a54068d89df62
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-11-13 22:40:12 +0530

    Time: 1942 ms (14.54%), Space: 30.7 MB (5.22%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -9,11 +9,11 @@ class Solution:
             if char in vowels:
                 vowels_l[index] = char
                 
-        print(vowels_l)
+        # print(vowels_l)
         
         v = sorted(vowels_l.values())
         k = sorted(vowels_l.keys())
-        print(v,k)
+        # print(v,k)
         
         for val,idx in zip(v,k):
             s = s[:idx] + val + s[idx+ 1 :]
