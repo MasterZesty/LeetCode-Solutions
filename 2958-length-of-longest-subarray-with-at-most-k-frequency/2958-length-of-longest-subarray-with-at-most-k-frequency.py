98e5046af4a98cc5846111fca8c681f47ea0edef
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2023-12-10 14:47:48 +0530

    Time: 1284 ms (100.00%), Space: 31.1 MB (100.00%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -23,39 +23,5 @@ class Solution:
             max_length = max(max_length, right - left + 1)
 
         return max_length
-        
-#         # step 1 : do pre processing
-        
-#         m = len(nums)
-        
-#         running_max_k = [ 0 for i in range(m)]
-#         running_max_k[0] = 1
-        
-#         hm = {}
-        
-#         for i,n in enumerate(nums):
-            
-#             hm[n] = hm.get(n,0) + 1
-            
-#             t = hm.get(n,0)
-            
-#             if t > running_max_k[i-1] and i != 0:
-#                 running_max_k[i] = t
-                
-#             elif i !=0:
-#                 running_max_k[i] = running_max_k[i-1]
-                
-#         print(f"running_max_k {running_max_k}")
-        
-#         start = running_max_k.index(1)
-#         end = 0
-        
-#         for i,n in enumerate(running_max_k):
-#             if n <= k:
-#                 end = i
-                
-#         print(f"start : {start} end: {end} {nums[start]}| ans is : {nums[start:end+1]}")
-        
-#         return len(nums[start:end+1])
             
         
