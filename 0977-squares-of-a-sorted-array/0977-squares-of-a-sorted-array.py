506cb7ef28a54ab92442fe081130d2883f8933a9
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2024-03-02 22:39:51 +0530

    Time: 158 ms (50.27%), Space: 18.9 MB (41.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         '''
-        Approach 1: TC - O( n + log n ) SC - O(1)
+        Approach 1: TC - O( nlog n ) SC - O(1)
         '''
         
         n = len(nums)
@@ -17,7 +17,8 @@ class Solution:
         '''
         
         '''
-        Approach 2 : TC - O(log n) SC - O(n)
+        Approach 2 : TC - O(nlog n) SC - O(n)
+        '''
         '''
         import heapq
         
@@ -27,4 +28,34 @@ class Solution:
             heapq.heappush(sqsort, nums[i]*nums[i])
         
         return [heapq.heappop(sqsort) for i in range(n)]
+        '''
+        
+        '''
+        Approach 3 : two pointer method
+        '''
+        l = 0
+        r = n - 1
+        i = n - 1
+        ans = [0]*n
+        
+        while l <= r:
+            l_sq = nums[l]*nums[l]
+            r_sq = nums[r]*nums[r]
+            
+            if l_sq >= r_sq:
+                ans[i] = l_sq
+                l += 1
+            else:
+                ans[i] = r_sq
+                r -= 1
+            
+            i -= 1
+        
+        return ans
+        
+        
+        
+        
+        
+        
         
