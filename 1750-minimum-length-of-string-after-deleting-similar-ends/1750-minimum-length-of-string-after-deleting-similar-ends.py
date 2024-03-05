c04ab9c90e4aac1ec711dec475a8e2ec8b272d2f
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2024-03-05 17:48:00 +0530

    Time: 76 ms (41.23%), Space: 17.5 MB (53.55%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -1,5 +1,14 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
+        '''
+        We are asked to find the minimum possible length of the string after performing the given operation
+        any number of times on a given string. The operation is to remove a prefix and a suffix from the
+        string if and only if both are formed from the same characters. Note that both the prefix and
+        suffix must be of non-zero length. This implies that if length of the string is 1, or the first
+        and last characters of the string are different, we cannot do further operations on it.
+        TC : O(n) where n is lenght of string
+        SC : O(1)
+        '''
         n = len(s)
         l = 0
         r = n -1
