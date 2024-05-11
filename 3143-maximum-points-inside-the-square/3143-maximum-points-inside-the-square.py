c4fe85c0f102b6cced618f331b348d220261b44e
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2024-05-11 22:46:56 +0530

    Time: 899 ms (100.00%), Space: 60.1 MB (100.00%) - LeetHub

--- 3143-maximum-points-inside-the-square/3143-maximum-points-inside-the-square.py
+++ 3143-maximum-points-inside-the-square/3143-maximum-points-inside-the-square.py
@@ -1,5 +1,22 @@
 class Solution:
     def maxPointsInsideSquare(self, points: List[List[int]], s: str) -> int:
+        # ref : https://leetcode.com/problems/maximum-points-inside-the-square/discuss/5144001/Simple-Maths-Explanation-Python3-O(nlog(n))
+        
+        """
+        explanation: 
+        1. let us say we have n points in array. now if we consider first point
+           then the locaii of that point to form a reactangle should be
+           r = max(abs(x), abs(y))
+        2. we get such r1, r2, r3 radius consider it as ripple but rectangular 
+           ripple
+        3. now each of this ripple will number of tags assciated with it beacause
+           radius r of this ripple is calculated from that point pi(xi,yi) which have
+           tag s[i] assciated with it
+        4. so will be having number of rectangular ripples with some tags with it
+        5. will sort this ripple frm smaller to larger and will check at each ripple
+           if tag is repeated (will store track of tags in set) if it repeated then will
+           return count till that point
+        """
         
         hm = defaultdict(list)
         
