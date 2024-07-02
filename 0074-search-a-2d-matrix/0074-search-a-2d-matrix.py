4868bf9af022161649fc611ef1fc20e15d504f97
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2024-07-03 00:26:07 +0530

    Time: 50 ms (26.21%), Space: 17 MB (55.00%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,6 +1,9 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
+        '''
+        TC - O(mlogn) ==> O(log(m * n)) (flattening the matrix virtually)
+        '''
+        '''
         def bs(arr, key):
             n = len(arr)
             i = 0
@@ -27,4 +30,27 @@ class Solution:
                 return True
             
             
+        return False
+        '''
+        
+        m = len(matrix)
+        n = len(matrix[0])
+
+        i = 0
+        j = m*n - 1
+        
+        while i <= j:
+            
+            mid = (i+j)//2
+            mid_value = matrix[mid // n][mid % n]
+            
+            if mid_value == target:
+                return True
+            
+            if mid_value > target:
+                j = mid - 1
+                
+            if mid_value < target:
+                i = mid + 1
+                
         return False
