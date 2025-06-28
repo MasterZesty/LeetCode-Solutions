0f5fdc22596d83613b157a94d2cad7a35c3b3276
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2025-06-28 20:35:06 +0530

    Time: 379 ms (60.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+with stg as
+(
+select
+emp.name as emp_name,
+emp.salary as emp_sal,
+mgr.salary as mgr_sal
+from employee emp
+left join employee mgr
+on emp.managerid = mgr.id
+)
+
+select emp_name as Employee  from stg 
+where 
+emp_sal > mgr_sal
