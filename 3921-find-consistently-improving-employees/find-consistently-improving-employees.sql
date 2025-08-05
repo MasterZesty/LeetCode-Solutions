2cbd588ced5d8b5956339acbff8141e4aa458631
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2025-08-05 22:53:20 +0530

    Time: 712 ms (7.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3921-find-consistently-improving-employees/find-consistently-improving-employees.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+with review_no as (
+select
+employee_id,
+NTH_VALUE(rating, 1) over(partition by employee_id order by review_date desc range between unbounded preceding and unbounded following) as latest_reviw_rating,
+NTH_VALUE(rating, 2) over(partition by employee_id order by review_date desc range between unbounded preceding and unbounded following) as second_latest_reviw_rating,
+NTH_VALUE(rating, 3) over(partition by employee_id order by review_date desc range between unbounded preceding and unbounded following) as third_latest_reviw_rating,
+row_number() over(partition by employee_id) as rn
+from
+performance_reviews
+)
+
+select
+review_no.employee_id ,
+emp.name,
+latest_reviw_rating - third_latest_reviw_rating as improvement_score
+from 
+review_no
+inner join employees emp
+on emp.employee_id = review_no.employee_id
+where rn = 1 
+and latest_reviw_rating is not null
+and second_latest_reviw_rating is not null
+and third_latest_reviw_rating is not null
+and latest_reviw_rating > second_latest_reviw_rating
+and second_latest_reviw_rating > third_latest_reviw_rating
+order by improvement_score desc, name asc
