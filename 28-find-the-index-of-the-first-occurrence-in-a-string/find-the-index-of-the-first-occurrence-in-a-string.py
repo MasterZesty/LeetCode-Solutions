5a1d8c0184d776025f2fe7a012cf33cf15deadb1
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2026-06-08 19:43:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (60.63%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,17 +3,8 @@ class Solution:
         # sol 1 - brute force
         n = len(haystack)
         m = len(needle)
-        # print(n,m)
         for i in range(0,n-m+1):
-            string_match = True
-            # print(i,string_match)
-            for m, c in enumerate(needle):
-                print(m,c)
-                if c != haystack[m+i]:
-                    string_match = False
-
-            if string_match:
+            if haystack[i:i+m] == needle:
                 return i
-
 
         return -1
