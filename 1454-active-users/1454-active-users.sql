a2d2ec64f9c1bab837e8a3377b09d16ee793f0b1
Krishna Nimbalkar <58479910+MasterZesty@users.noreply.github.com>
2026-08-20 01:57:39 +0530

    Time: 1510 ms (57.05%), Space: 0B (100%) - LeetHub

--- 1454-active-users/1454-active-users.sql
+++ 1454-active-users/1454-active-users.sql
@@ -38,45 +38,86 @@
 -- DATE_ADD(4_login, INTERVAL 1 DAY) = 5_login
 
 
+-- Apporch 2: magic even i will not understand in future what is this spell i catsed at 2 in the night
+-- with count_of_daily_logins as (
+-- select
+-- id,
+-- login_date,
+-- count(login_date) as daily_login_count
+-- from
+-- logins
+-- group by
+-- id,
+-- login_date
+-- )
 
-with count_of_daily_logins as (
-select
-id,
-login_date,
-count(login_date) as daily_login_count
-from
-logins
-group by
-id,
-login_date
+-- , magic_abomination as (
+-- select
+-- id,
+-- login_date,
+-- -- MIN(login_date) OVER(PARTITION BY id) as min_login_date,
+-- DATEDIFF(DATE_ADD(MIN(login_date) OVER(PARTITION BY id), INTERVAL ROW_NUMBER() OVER(PARTITION BY id ORDER BY login_date)DAY), login_date) as grp_dif
+-- from
+-- count_of_daily_logins
+-- )
+
+-- , abra_ka_dabra as (
+-- select
+-- id, 
+-- count(grp_dif) as continue_logins
+-- from
+-- magic_abomination
+-- group by
+-- id,
+-- grp_dif
+-- having continue_logins >= 5 -- n can be anything
+-- )
+
+-- select distinct
+-- a.id,
+-- b.name
+-- from abra_ka_dabra a
+-- inner join Accounts b
+-- on a.id = b.id
+-- order by id
+
+-- Apporch 3 : this is what gpt suggested
+WITH daily_logins AS (
+    SELECT DISTINCT
+        id,
+        login_date
+    FROM logins
+),
+
+login_sequence AS (
+    SELECT
+        id,
+        login_date,
+        DATE_SUB(
+            login_date,
+            INTERVAL ROW_NUMBER() OVER (
+                PARTITION BY id
+                ORDER BY login_date
+            ) DAY
+        ) AS grp
+    FROM daily_logins
+),
+
+login_streaks AS (
+    SELECT
+        id,
+        grp
+    FROM login_sequence
+    GROUP BY
+        id,
+        grp
+    HAVING COUNT(*) >= 5
 )
 
-, magic_abomination as (
-select
-id,
-login_date,
--- MIN(login_date) OVER(PARTITION BY id) as min_login_date,
-DATEDIFF(DATE_ADD(MIN(login_date) OVER(PARTITION BY id), INTERVAL ROW_NUMBER() OVER(PARTITION BY id ORDER BY login_date)DAY), login_date) as grp_dif
-from
-count_of_daily_logins
-)
-
-, abra_ka_dabra as (
-select
-id, 
-count(grp_dif) as continue_logins
-from
-magic_abomination
-group by
-id,
-grp_dif
-having continue_logins >= 5 -- n can be anything
-)
-
-select distinct
-a.id,
-b.name
-from abra_ka_dabra a
-inner join Accounts b
-on a.id = b.id
-order by id
+SELECT DISTINCT
+    s.id,
+    a.name
+FROM login_streaks s
+JOIN accounts a
+    ON s.id = a.id
+ORDER BY s.id
